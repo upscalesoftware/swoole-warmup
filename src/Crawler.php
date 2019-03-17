f14c939ec94c4bd2f56492814f4564550ed17585
Sergii Shymko <sergey@shymko.net>
2019-03-16 22:43:40 -0700

    Fix server middleware detection
    - Fix incompatibility with Swoole 4.3
    - Recognize middleware on server and port levels
    - Retrieve middleware by breaking encapsulation

--- src/Crawler.php
+++ src/Crawler.php
@@ -42,7 +42,7 @@ class Crawler
     }
 
     /**
-     * Visit a given URL and return the response
+     * Visit a given URL and fetch the response
      * 
      * @param string $url
      * @return \Swoole\Http\Response
@@ -63,12 +63,62 @@ class Crawler
      */
     public function dispatch(\Swoole\Http\Request $request)
     {
-        $middleware = $this->server->onRequest;
-        if (!is_callable($middleware)) {
-            throw new \UnexpectedValueException('Server middleware has not been initialized yet.');
-        }
+        $middleware = $this->getMiddleware($this->server);
         $response = new Response();
         $middleware($request, $response);
         return $response;
+    }
+
+    /**
+     * Retrieve a middleware handling requests of a given server
+     * 
+     * @param \Swoole\Http\Server $server
+     * @return callable
+     * @throws \UnexpectedValueException
+     */
+    protected function getMiddleware(\Swoole\Http\Server $server)
+    {
+        $middleware = $this->getCallback($server, 'request')
+            ?: $this->getCallback($this->getPrimaryPort($server), 'request');
+        if (!is_callable($middleware)) {
+            throw new \UnexpectedValueException('Server middleware has not been detected.');
+        }
+        return $middleware;
+    }
+
+    /**
+     * Retrieve the primary port listened by a given server
+     * 
+     * @param \Swoole\Http\Server $server
+     * @return \Swoole\Server\Port
+     * @throws \UnexpectedValueException 
+     */
+    protected function getPrimaryPort(\Swoole\Http\Server $server)
+    {
+        foreach ((array)$server->ports as $port) {
+            if ($port->host == $server->host && $port->port == $server->port) {
+                return $port;
+            }
+        }
+        throw new \UnexpectedValueException('Server port has not been identified.');
+    }
+
+    /**
+     * Retrieve a callback subscribed to a given event
+     * 
+     * @param object $observable
+     * @param string $eventName
+     * @return callable|null
+     */
+    protected function getCallback($observable, $eventName)
+    {
+        try {
+            $propertyName = 'on' . ucfirst($eventName);
+            $property = new \ReflectionProperty($observable, $propertyName);
+            $property->setAccessible(true);
+            return $property->getValue($observable);
+        } catch (\ReflectionException $e) {
+            return null;
+        }
     }
 }
